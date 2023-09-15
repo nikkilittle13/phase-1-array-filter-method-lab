41f0c132a57315ae726d8a55b829c5d4a9401099
Nikki Little <nikkilittle13@gmail.com>
2023-09-15 11:14:10 -0400

    creates myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution here
+function findMatching(drivers, searchString) {
+    const lowercaseSearchString = searchString.toLowerCase();
+    return drivers.filter(driver => driver.toLowerCase() === lowercaseSearchString);
+}
+
+function fuzzyMatch(drivers, query) {
+    const matchingDrivers = [];
+
+    for (let i = 0; i < drivers.length; i++) {
+        if (drivers[i].startsWith(query)) {
+            matchingDrivers.push(drivers[i]);
+        }
+    }    
+    return matchingDrivers;
+}
+
+function matchName(drivers, searchString) {
+    const matchingDrivers = [];
+
+    drivers.forEach(driver => {
+        if (driver.name === searchString) {
+            matchingDrivers.push(driver);
+        }
+    });
+
+    return matchingDrivers;
+}
